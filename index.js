e81dea2028923dcadfb4bbbed91ca295d5539c2d
Saravanakumar1802 <saravana1802kumar@gmail.com>
2023-12-06 15:29:17 +0530

    cors fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://money-manager-app-saravana.netlify.app',
     credentials: true,
     optionSuccessStatus: 200
 }
